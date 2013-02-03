6fe79f5c72091fdff5668bc3f6e4b480b4f5b3c8
Yann <comte.yannick@gmail.com>
2013-02-03 15:32:18 +0100

    Added base class for StorageManager

---
+++ Engine/StorageManager.js
@@ -0,0 +1,15 @@
+var Atlantis = window.Atlantis || {};
+
+(function () {
+    Atlantis.StorageManager = function () {
+        
+    };
+
+    Atlantis.StorageManager.prototype.save = function (name, data, container) {
+        
+    };
+
+    Atlantis.StorageManager.prototype.load = function (name, container) {
+        
+    };
+})();
