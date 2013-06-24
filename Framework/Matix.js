d34d9a8d58e512ae2fcab73621d8cca2a75dde75
Yann <comte.yannick@gmail.com>
2013-06-24 08:22:10 +0200

    New methods on Matrix class, all is almost done to start the 3D soft renderer

--- Framework/Matix.js
+++ Framework/Matix.js
@@ -97,6 +97,34 @@ Atlantis.Matrix = (function () {
     };
 
     /**
+    * Add a matrix to this matrix.
+    * @param matrix A matrix to add.
+    */
+    matrix.prototype.add = function (matrix) {
+        var mValues = this.toArray();
+        var eValues = matrix.toArray();
+
+        for (var i = 0; i < 16; i++) {
+            mValues[i] += eValues[i];
+        }
+        this.set(mValues);
+    }
+
+    /**
+    * Add two matrix.
+    * @method add
+    * @static
+    * @param {Atlantis.Matrix} matA A matrix
+    * @param {Atlantis.Matrix} matB Another matrix to add with the first
+    * @return {Atlantis.Matrix} Return a new matrix.
+    */
+    matrix.add = function (matA, matB) {
+        var matrix = new Atlantis.Matrix(matA);
+        matrix.add(matB);
+        return matrix;
+    };
+
+    /**
     * Sets translation.
     * @method setTranslation
     * @param {Atlantis.Vetor3} position The position to set.
@@ -164,6 +192,273 @@ Atlantis.Matrix = (function () {
         matrix.M33 = cos;
 
         return matrix;
+    };
+
+    /**
+    * Create a scale matrix.
+    * @method createScale
+    * @static
+    * @param {Number} sx Desired scale on X axis.
+    * @param {Number} sy Desired scale on Y axis.
+    * @param {Number} sz Desired scale on Z axis.
+    * @return {Atlantis.Matrix} Return a scale matrix.
+    */
+    matrix.createScale = function (sx, sy, sz) {
+        var matrix = Atlantis.Matrix.getMatrixIdentity();
+        matrix.M11 = sx;
+        matrix.M22 = sy || sx;
+        matrix.M33 = sz || sx;
+        return matrix;
+    };
+
+    /**
+    * Create a translation matrix.
+    * @method createTranslation
+    * @static
+    * @param x Position on X axis.
+    * @param y Position on Y axis.
+    * @param z Position on Z axis.
+    * @return Return a matrix translation.
+    */
+    matrix.createTranslation = function (x, y, z) {
+        var matrix = Atantis.Matrix.getMatrixIdentity();
+        matrix.M41 = x;
+        matrix.M42 = y || x;
+        matrix.M43 = z || x;
+        return matrix;
+    };
+
+    /**
+    * Create a view matrix.
+    * @metod createLookAt
+    * @static
+    * @param {Atlantis.Vector3} position The position of the camera.
+    * @param Atlantis.Vector3} target The target of the camera.
+    * @param Atlantis.Vector3} upVector Vector up
+    * @return {Atlantis.Matrix} Return a view camera.
+    */
+    matrix.createLookAt = function (position, target, upVector) {
+        var zAxis = Atlantis.Vector3.subtract(target, position);
+        zAxis.normalize();
+        var xAxis = Atlantis.Vector3.cross(upVector, zAxis);
+        xAxis.normalize();
+        var yAxis = Atlantis.Vector3.cross(zAxis, xAxis);
+        yAxis.normalize();
+
+        var matrix = Atlantis.Matrix.getMatrixIdentity();
+
+        matrix.M11 = xAxis.x;
+        matrix.M21 = xAxis.y;
+        matrix.M31 = xAxis.z;
+
+        matrix.M12 = yAxis.x;
+        matrix.M22 = yAxis.y;
+        matrix.M32 = yAxis.z;
+
+        matrix.M13 = zAxis.x;
+        matrix.M23 = zAxis.y;
+        matrix.M33 = zAxis.z;
+
+        matrix.M41 = -Atlantis.Vector3.dot(xAxis, position);
+        matrix.M42 = -Atlantis.Vector3.dot(yAxis, position);
+        matrix.M43 = -Atlantis.Vector3.dot(zAxis, position);
+
+        return matrix;
+    };
+
+   /**
+	 * Create a perspective field of view matrix with Left hand notation.
+     * @method createPerspectiveFieldOfView
+     * @static
+	 * @param {Number} fov Desired field of view (Math.PI / 4 is a good value)
+	 * @param {Number} aspect Desired aspect ratio (Screen width / height)
+	 * @param {Number} near Near clip
+	 * @param {Number} far Far clip
+	 * @return {Atlantis.Matrix} Return a matrix of this type of perspective.
+	 */
+	matrix.createPerspectiveFieldOfView = function (fov, aspect, zNear, zFar) {
+		var yScale = (float)(1.0 / Math.tan(fov * 0.5f));
+		var xScale = yScale / aspect;
+		var halfWidth = zNear / xScale;
+		var halfHeight = zNear / yScale;
+		
+		return Atlantis.Matrix.createPerspectiveOffCenter(-halfWidth, halfWidth, -halfHeight, halfHeight, zNear, zFar);
+	};
+	
+	/**
+	 * Create a perspective field of view matrix with Right hand notation.
+     * @method createPerspectiveFieldOfViewRH
+     * @static
+	 * @param {Number} fov Desired field of view (Math.PI / 4 is a good value)
+	 * @param {Number} aspect Desired aspect ratio (Screen width / height)
+	 * @param {Number} near Near clip
+	 * @param {Number} far Far clip
+	 * @return {Atlantis.Matrix} Return a matrix of this type of perspective.
+	 */
+	matrix.createPerspetiveFieldOfViewRH = function (fov, aspect, zNear, zFar) {
+		var matrix = Atlantis.Matrix.createPerspectiveFieldOfView(fov, aspect, zNear, zFar);
+		matrix.M31 *= -1.0;
+		matrix.M32 *= -1.0;
+		matrix.M33 *= -1.0;
+		matrix.M34 *= -1.0;
+		return matrix;
+	};
+
+    /**
+    * Create a custom perspective matrix.
+    * @method createPerspectiveOffCenter
+    * @static
+    * @param {Number} left Minimum X value of the viewing volume.
+    * @param {Number} right Maximum X value of the viewing volume.
+    * @param {Number} bottom Minimum Y value of the viewing volume.
+    * @param {Number} top Maximum Y value of the viewing volume.
+    * @param {Number} zNear Minimum Z value of the viewing volume.
+    * @param {Number} zFar Maximum Z value of the viewing volume.
+    * @return {Atlantis.Matrix} Return a new custom perspective matrix.
+    */
+    matrix.createPerspectiveOffCenter = function (left, right, bottom, top, zNear, zFar) {
+        var zRange = zFar / (zFar - zNear);
+        var matrix = new Atlantis.Matrix();
+        matrix.M11 = 2.0 * zNear / (right - left);
+        matrix.M22 = 2.0 * zNear / (top - bottom);
+        matrix.M31 = (left + right) / (left - right);
+        matrix.M32 = (top + bottom) / (bottom - top);
+        matrix.M33 = zRange;
+        matrix.M34 = 1.0;
+        matrix.M43 = -zNear * zRange;
+        return matrix;
+    };
+
+    /**
+	 * Create a world matrix.
+     * @method createWorld
+     * @static
+	 * @param {Atlantis.Vector3} position
+	 * @param {Atlantis.Vector3} forward
+	 * @param {Atlantis.Vector3} upVector
+	 * @return {Atlantis.Matrix} Return a world matrix.
+	 */
+	matrix.createWorld = function (position, forward, upVector) {
+		var matrix = new Atlantis.Matrix();
+		
+		var x = Atlantis.Vector3.cross(forward, upVector);
+		var y = Atlantis.Vector3.cross(x, forward);
+		var z = Atlantis.Vector3.normalize(forward);
+		x.normalize();
+		y.normalize();
+		
+		//matrix.setRight(x);
+		//matrix.setUp(y);
+		//matrix.setForward(z);
+		matrix.setTranslation(position);
+		matrix.M44 = 1.0;
+		
+		return matrix;
+	}
+
+    /**
+    * Multiply this matrix by another matrix.
+    * @method multiply
+    * @param {Atlantis.Matrix} matrix A matrix to multiply.
+    */
+    matrix.prototype.multiply = function (matrix) {
+        var m11 = (((this.M11 * matrix.M11) + (this.M12 * matrix.M21)) + (this.M13 * matrix.M31)) + (this.M14 * matrix.M41);
+        var m12 = (((this.M11 * matrix.M12) + (this.M12 * matrix.M22)) + (this.M13 * matrix.M32)) + (this.M14 * matrix.M42);
+        var m13 = (((this.M11 * matrix.M13) + (this.M12 * matrix.M23)) + (this.M13 * matrix.M33)) + (this.M14 * matrix.M43);
+        var m14 = (((this.M11 * matrix.M14) + (this.M12 * matrix.M24)) + (this.M13 * matrix.M34)) + (this.M14 * matrix.M44);
+        var m21 = (((this.M21 * matrix.M11) + (this.M22 * matrix.M21)) + (this.M23 * matrix.M31)) + (this.M24 * matrix.M41);
+        var m22 = (((this.M21 * matrix.M12) + (this.M22 * matrix.M22)) + (this.M23 * matrix.M32)) + (this.M24 * matrix.M42);
+        var m23 = (((this.M21 * matrix.M13) + (this.M22 * matrix.M23)) + (this.M23 * matrix.M33)) + (this.M24 * matrix.M43);
+        var m24 = (((this.M21 * matrix.M14) + (this.M22 * matrix.M24)) + (this.M23 * matrix.M34)) + (this.M24 * matrix.M44);
+        var m31 = (((this.M31 * matrix.M11) + (this.M32 * matrix.M21)) + (this.M33 * matrix.M31)) + (this.M34 * matrix.M41);
+        var m32 = (((this.M31 * matrix.M12) + (this.M32 * matrix.M22)) + (this.M33 * matrix.M32)) + (this.M34 * matrix.M42);
+        var m33 = (((this.M31 * matrix.M13) + (this.M32 * matrix.M23)) + (this.M33 * matrix.M33)) + (this.M34 * matrix.M43);
+        var m34 = (((this.M31 * matrix.M14) + (this.M32 * matrix.M24)) + (this.M33 * matrix.M34)) + (this.M34 * matrix.M44);
+        var m41 = (((this.M41 * matrix.M11) + (this.M42 * matrix.M21)) + (this.M43 * matrix.M31)) + (this.M44 * matrix.M41);
+        var m42 = (((this.M41 * matrix.M12) + (this.M42 * matrix.M22)) + (this.M43 * matrix.M32)) + (this.M44 * matrix.M42);
+        var m43 = (((this.M41 * matrix.M13) + (this.M42 * matrix.M23)) + (this.M43 * matrix.M33)) + (this.M44 * matrix.M43);
+        var m44 = (((this.M41 * matrix.M14) + (this.M42 * matrix.M24)) + (this.M43 * matrix.M34)) + (this.M44 * matrix.M44);
+
+        this.M11 = m11;
+        this.M12 = m12;
+        this.M13 = m13;
+        this.M14 = m14;
+        this.M21 = m21;
+        this.M22 = m22;
+        this.M23 = m23;
+        this.M24 = m24;
+        this.M31 = m31;
+        this.M32 = m32;
+        this.M33 = m33;
+        this.M34 = m34;
+        this.M41 = m41;
+        this.M42 = m42;
+        this.M43 = m43;
+        this.M44 = m44;
+    };
+
+    /**
+    * Multiply a two matrix.
+    * @method multiply
+    * @static
+    * @param {Atlantis.Matrix} matrixA A matrix.
+    * @param {Atlantis.Matrix} matrixB Another matrix.
+    * @param {Atlantis.Matrix} matrixC Another matrix (optional).
+    * @return {Atlantis.Matrix} Return a new matrix.
+    */
+    matrix.multiply = function (matrixA, matrixB, matrixC) {
+        var matrix = new Atlantis.Matrix(matrixA);
+        matrix.multiply(matrixB);
+
+        if (matrixC instanceof Atlantis.Matrix) {
+            matrix.multiply(matrixC);
+        }
+
+        return matrix;
+    };
+
+    /**
+    * Subtract a matrix to this matrix.
+    * @method subtract
+    * @param {Atlantis.Matrix} matrix A matrix to add.
+    */
+    matrix.prototype.subtract = function (matrix) {
+        var mValues = this.toArray();
+        var eValues = matrix.toArray();
+
+        for (var i = 0; i < 16; i++) {
+            mValues[i] -= eValues[i];
+        }
+        this.set(mValues);
+    }
+
+    /**
+    * Subtract two matrix.
+    * @method subtract
+    * @static
+    * @param {Atlantis.Matrix} matA A matrix.
+    * @param {Atlantis.Matrix} matB Another matrix to use to subtract with the first matrix.
+    * @return {Atlantis.Matrix} Return a new matrix.
+    */
+    matrix.subtract = function (matA, matB) {
+        var mat = new Atlantis.Matrix(matA);
+        mat.subtract(matB);
+        return mat;
+    }
+
+    matrix.prototype.toString = function () {
+        var values = this.toArray();
+        var builder = [];
+
+        for (var i = 0; i < 16; i += 4) {
+            builder.push("[");
+            builder.push(values[i] + " ");
+            builder.push(values[i + 1] + " ");
+            builder.push(values[i + 2] + " ");
+            builder.push(values[i + 3]);
+            builder.push("] ");
+        }
+        return builder.join("");
     };
 
     return matrix;
