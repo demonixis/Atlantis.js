c89180b05f63e813b998b4ad8266c507952c883b
Yannick Comte <ycomte@wanadev.fr>
2013-06-25 14:02:25 +0200

    Added new page

---
+++ doc/api.js
@@ -0,0 +1,50 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Atlantis.AudioManager",
+        "Atlantis.ContentManager",
+        "Atlantis.DrawableGameComponent",
+        "Atlantis.Engine",
+        "Atlantis.Entity",
+        "Atlantis.Game",
+        "Atlantis.GameComponent",
+        "Atlantis.GameTime",
+        "Atlantis.Graphics",
+        "Atlantis.Helpers",
+        "Atlantis.Interpolator",
+        "Atlantis.KeyboardState",
+        "Atlantis.Layer",
+        "Atlantis.MouseState",
+        "Atlantis.Point",
+        "Atlantis.Rectangle",
+        "Atlantis.Sprite",
+        "Atlantis.SpriteGroup",
+        "Atlantis.State",
+        "Atlantis.StorageManager",
+        "Atlantis.Tile",
+        "Atlantis.Tilemap",
+        "Atlantis.Vector2"
+    ],
+    "modules": [
+        "Atlantis",
+        "Engine",
+        "Framework"
+    ],
+    "allModules": [
+        {
+            "displayName": "Atlantis",
+            "name": "Atlantis"
+        },
+        {
+            "displayName": "Engine",
+            "name": "Engine",
+            "description": "Atlantis storage manager."
+        },
+        {
+            "displayName": "Framework",
+            "name": "Framework",
+            "description": "Atlantis storage manager."
+        }
+    ]
+} };
+});
